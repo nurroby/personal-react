b97e5e8da71415650c15be54ea052dd16a6ec825
Nurroby Wahyu Saputra <inbox.ibor@gmail.com>
2021-10-11 15:40:54 +0700

    🚧 remove static pdf resume

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,7 @@
-import resume from '../assets/pdf/resume.pdf'
-
 export const headerData = {
     name: 'Nurroby WS',
     title: "IT Enthutiast",
     desciption:"Hey, are you looking developer to build your web and grow your bussiness? let's shake hands with me..",
     image: 'https://media-exp1.licdn.com/dms/image/C5603AQEwFaJwLFxAZQ/profile-displayphoto-shrink_800_800/0/1603266737930?e=1639612800&v=beta&t=3_KPif0KWhKRxu_p-1ZDDpkf2HnfbeekoDGQu-7pHW8',
-    resumePdf: resume
+    resumePdf: 'https://s.id/nurroby-resume'
 }
